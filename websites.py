917d521c2ef0fdabbf4b0caf6c32d9d4ec822b6b
Constantin Bilz <constantin.bilz@gmail.com>
2017-05-12 20:28:43 +0200

    Define cache directory in config file

--- websites.py
+++ websites.py
@@ -1,5 +1,7 @@
+from os.path import expanduser
 from website import Website, Check, Check_tag_name, Pdfs_check, Notifier, Mail_notifier
 
+cache_dir = expanduser("~/.websitecheck")
 
 wi = Website("weighted_inequalities", "http://www.math.uni-bonn.de/ag/ana/SoSe2017/weights/", [Check("col1_content")], [Notifier()])
 aae = Website("app_alg_exercise", "http://www.or.uni-bonn.de/lectures/ss17/appr_ss17_ex.html", [Check_tag_name("body")], [Notifier()])
