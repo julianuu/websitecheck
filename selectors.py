70f8f72c7443226bc3714bd9df8829c0216ad27d
Constantin Bilz <constantin.bilz@gmail.com>
2017-05-19 21:18:01 +0200

    First reasonable version of selectors.py

--- selectors.py
+++ selectors.py
@@ -1,15 +1,28 @@
-from abc import ABC, abstractmethod
+class Selector:
+    # Select something from the given data, which is a list of
+    # BeautifulSoup's. Must not modify the argument, but return
+    # a NEW list.
+    def select(data):
+        raise NotImplementedError("Please implement this method")
 
-class Selector(ABC):
-    @abstractmethod
-    def select():
-        pass
+    # Return a string which will be used to compose the directory
+    # name associated to a query.
+    def __repr__(self):
+        raise NotImplementedError("Please implement this method")
 
-def selectors_to_string(selectors):
-    strings = [selector[0] for selector in selectors]
-    return ",".join(strings)
 
-def id(tag_id):
-    def select_by_id(soup):
-        return soup.find(id=tag_id)
-    return ["id='" + tag_id + "'", select_by_idi]
+# Selects the first tag with the given id.
+# (Html id's should be unique in a document.)
+class Id(Selector):
+    def __init__(self, html_id):
+        self.html_id = html_id
+
+    def select(self, data):
+        for soup in data:
+            el = soup.find(id=self.html_id)
+            if el != None:
+                return [el]
+        return []
+
+    def __repr__(self):
+        return 'id(' + self.html_id + ')'
