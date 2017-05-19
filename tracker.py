2a7ae2b1ab38093f03739589b5a11664cfb311c1
Constantin Bilz <constantin.bilz@gmail.com>
2017-05-19 20:54:36 +0200

    First reasonable version of tracker.py

--- tracker.py
+++ tracker.py
@@ -1,86 +1,73 @@
-import urllib.request   # for fetching websites
+from config import cache_dir    # directory of websitecheck's cache
+
+import urllib.request  # for fetching websites
 '''
     BeautifulSoup is not in the Python standard library.
     Need to install python-beautifulsoup4 in Arch Linux.
 '''
 from bs4 import BeautifulSoup   # for HTML parsing
-from difflib import HtmlDiff    # for creating nice visual diffs
 
-# for email functionality:
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-from getpass import getpass # for entering passwords
-
-from os import listdir
-from os.path import join, isfile   # for determining file and directory paths
-
-import re   # regular expressions
-
-#look for links to pdf within tag, check them for change
-def find_pdflinks(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    pdflink_tags = soup.find_all(re.compile('([^/]/)*[^/]*\.pdf'))
-    pdflinks = []
-    for tag in pdflink_tags:
-        link = tag['href']
-        #find the *.pdf and call it 'name'
-        match = re.search('([^/]/)*(?P<name>[^/]*\.pdf)',link)
-        pdflinks.append([match.group['name'], name])
-    return pdflinks
+# for file system operations
+from os import listdir, remove
+from os.path import isfile
+from os.path import join as join_path
 
 
-class Pdfs_check(Check):
-    def difference(self, old, new):
-        pdflinks_old = find_pdflinks(old)
-        pdflinks_new = find_pdflinks(new)
-        return Notification()
-
-
+# Tracks a (part of a) Website
 class Tracker:
     def __init__(self, name, url, actions):
         self.name = name
         self.url = url
         self.actions = actions
-    
-    def check(self):
-        with urllib.request.urlopen(self.url) as response:
-            new_data = [BeautifulSoup(response, 'html.parser')]
 
-        self.__iterate__(new_data, [], self.actions)
+
+    def check(self):
+        # fetch new data
+        with urllib.request.urlopen(self.url) as response:
+            data = [BeautifulSoup(response, 'html.parser')]
+
+        # start the recursion
+        self.__iterate__(data, [], self.actions)
 
 
     def __iterate__(data, selectionsDone, actionsPending):
         if len(actionsPending) == 0:
-            return
+            return  # leave because we are at a leaf
 
         actions = actionsPending[0]
         if not isinstance(actions, list):
             actions = [actions]
 
-        appended = False
-        
-        for action in actions:
-            if isinstance(a, Selector):
-                my_data = a.select(data)
-                selectionsDone.append(action)
-            elif isinstance(a, Notifier):
+        for a in actions:
+            if isinstance(a, Selector):     # This action is a selector.
+                my_data = a.select(data)    # call thee selector
+                my_selectionsDone = selectionsDone + [a]
+            elif isinstance(a, Notifier):   # This action is a notifier.
                 my_data = data
+                my_selectionsdone = selectionsDone
 
-                selstr = '.'.join(list(map(lambda a: repr(a), actions)))
-                old_dir = os.path.join(cache_dir, self.url.replace("/", "%2F"), selstr)
+                # determine cache directory for the current query
+                selstr = '.'.join(list(map(lambda s: repr(s), selectionsDone)))
+                my_dir = join_path(cache_dir, self.url.replace("/", "%2F"), selstr)
 
-                old_paths = sorted([f for f in listdir(old_dir) if isfile(join(old_dir,f))], key=int)
+                # fetch old data
+                old_paths = sorted([join_path(my_dir, f) for f in listdir(my_dir)
+                    if isfile(join_path(my_dir,f))], key=int)
                 old_data = []
                 for p in old_paths:
                     with open(p, 'r') as old_file:
-                        old_data.append(BeautifulSoup(old_file.read(), 'html.parser'))
+                        old_data.append(BeautifulSoup(old_file, 'html.parser'))
 
-                a.notify(self, new_data, old_data)
+                # call the notifier
+                a.notify(self, selectionsDone, old_data, data)
 
-                # TODO: Save acquired data.
+                # remove old snippets from cache
+                for p in old_paths:
+                    remove(p)
 
-            self.__iterate__(my_data, selectionsDone, actionsPending[:-1])
-            if appended:
-                actionsDone.pop()
+                # store new snippets in cache
+                for idx, snippet in enumerate(data):
+                    with open(join_path(my_dir, str(idx), 'w') as new_file:
+                        new_file.write(snippet.prettify())
+
+            self.__iterate__(my_data, my_selectionsDone, actionsPending[:-1])
